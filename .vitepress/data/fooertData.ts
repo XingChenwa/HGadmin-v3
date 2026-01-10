49a64144cd72cf6b3855f1adb16bdda12d0730b0
XingChenwa <2377130783@qq.com>
2026-01-10 09:48:12 +0800

    更新

---
+++ .vitepress/data/fooertData.ts
@@ -0,0 +1,26 @@
+import type { FooterData } from '@theojs/lumen'
+
+export const Footer_Data: FooterData = {
+  author: { name: 'ikenxuan', link: 'https://github.com/ikenxuan' },
+  group: [
+    {
+      title: '框架',
+      icon: 'fa-solid fa-robot',
+      links: [
+        { name: 'Miao-Yunzai', href: 'https://github.com/yoimiya-kokomi/Miao-Yunzai' },
+        { name: 'TRSS-Yunzai', href: 'https://github.com/TimeRainStarSky/Yunzai' },
+        { name: 'Yunzai Next', href: 'https://github.com/yunzai-org/yunzaijs' },
+        { name: 'Karin', href: 'https://github.com/Karinjs/Karin' },
+      ]
+    },
+    {
+      title: '相关链接',
+      icon: 'fab fa-github',
+      links: [
+        { name: 'ICQQ', href: 'https://github.com/icqqjs/icqq' },
+        { name: '插件库', href: 'https://github.com/yhArcadia/Yunzai-Bot-plugins-index' },
+        { name: '解析库', href: 'https://github.com/ikenxuan/amagi' }
+      ]
+    },
+  ]
+}
